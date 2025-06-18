be3560e95ee18a08c23612f229e035aef4c5cddf
CybTk <cybtk29@gmail.com>
2025-06-18 12:14:53 +0700

    Update pengurusRoutes.js

--- routes/pengurusRoutes.js
+++ routes/pengurusRoutes.js
@@ -1,1 +1,9 @@
+// routes/pengurusRoutes.js
+const express = require('express');
+const router = express.Router();
+const pengurusController = require('../controllers/pengurusController');
 
+// Rute untuk dashboard pengurus
+router.get('/dashboard', pengurusController.showDashboard);
+
+module.exports = router; // <-- PASTIKAN BARIS INI ADA
